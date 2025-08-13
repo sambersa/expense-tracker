b3833d0c8ddb3574af354a0f7242b555d872b360
Sam Bersa <1sambersa@gmail.com>
2025-08-13 23:47:29 +0200

    feat: Add income management UI components
    - Created AddIncomeForm with inputs for source, amount, date, and icon
    - Added EmojiPickerPopup for selecting or changing icons
    - Added reusable Modal component for displaying forms or content
    - Enhanced styling and added extra CSS for better UI/UX
    - Performed debugging and UI refinements for Income overview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/Dashboard/Home.jsx';
 import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
 import UserProvider from './context/userContext';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
@@ -29,6 +30,15 @@ const App = () => {
           </Routes>
         </Router>
       </div>
+
+      <Toaster
+        toastOtions={{
+          className: "",
+          style: {
+            fontsize: '13px'
+          },
+        }}
+      />
     </UserProvider>
   );
 };
--- frontend/src/pages/Dashboard/Income.jsx
+++ frontend/src/pages/Dashboard/Income.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import DashBoardLayout from '../../components/layouts/DashBoardLayout';
+import IncomeOverview from '../../components/Income/IncomeOverview';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import Modal from '../../components/Modal';
+import AddIncomeForm from '../../components/Income/AddIncomeForm';
+import { toast } from 'react-toastify';
 
 const Income = () => {
-  return (
-    <div>Income</div>
-  )
-}
 
-export default Income
+  const [incomeData, setIncomeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+
+  // Get all income details
+  const fetchIncomeDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const response = await axiosInstance.get(API_PATHS.INCOME.GET_ALL_INCOME);
+      if (response.data) {
+        setIncomeData(response.data);
+      }
+    } catch (error) {
+      console.error('Something went wrong. Please try again.', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Handle Add Income
+  const handleAddIncome = async (income) => {
+    const { source, amount, date, icon } = income;
+
+    // Validation Checks
+    if (!source.trim()) {
+      toast.error("Source is required")
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      toast.error("Amount needs to be a valid number greater than 0.")
+      return;
+    }
+
+    if (!date) {
+      toast.error("Date is required");
+      return;
+    }
+
+    try {
+      await axiosInstance.post(API_PATHS.INCOME.ADD_INCOME, {
+        source,
+        amount,
+        date,
+        icon,
+      });
+
+      setOpenAddIncomeModal(false);
+      toast.success("Income added successfully. ");
+      fetchIncomeDetails();
+    } catch (error) {
+      console.error(
+        "Error adding income.",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+
+  // Delete Income
+  const deleteIncome = async (id) => {};
+
+  // Handling download income details
+  const handleDownloadIncomeDetails = async () => {};
+
+  useEffect(() => {
+    fetchIncomeDetails();
+  }, []);
+
+  return (
+    <DashBoardLayout activeMenu="Income">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div>
+            <IncomeOverview
+              transactions={incomeData}
+              onAddIncome={() => setOpenAddIncomeModal(true)}
+            />
+          </div>
+        </div>
+
+      <Modal
+        isOpen={openAddIncomeModal}
+        onClose={() => setOpenAddIncomeModal(false)}
+        title="Add Income"
+      > 
+        <AddIncomeForm onAddIncome={handleAddIncome} />
+      </Modal>
+      </div>
+    </DashBoardLayout>
+  );
+};
+
+export default React.memo(Income);
